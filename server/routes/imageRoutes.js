49fce4605a73d5bae689305f09af695d50b9a81b
Necro-sword <166520193+Necro-sword@users.noreply.github.com>
2024-11-22 18:17:05 +0530

    Update imageRoutes.js

--- server/routes/imageRoutes.js
+++ server/routes/imageRoutes.js
@@ -1,17 +1,10 @@
 import express from 'express'
 import { generateImage } from '../controllers/imageController.js'
-<<<<<<< HEAD
+
 import userAuth from '../middleware/auth.js'
 
 const imageRouter = express.Router()
 
 imageRouter.post('/generate-image', userAuth, generateImage)
-=======
-
-
-const imageRouter = express.Router()
-
-imageRouter.post('/generate-image', generateImage)
->>>>>>> 9ba7c15 (second commit)
 
 export default imageRouter;
